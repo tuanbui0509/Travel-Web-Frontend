450fb0a8a885b7aeebd9daf73b397c54d26a98b4
tuanbui0509 <tuanbui0509@gmail.com>
2021-04-03 18:52:27 +0700

    update error 1

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,9 @@
-window.onscroll = function(){
-    headerScroll();
-}
 
 var header_nav = document.getElementById('header-nav')
 var sticky = header_nav.offsetTop;
-console.log(sticky)
-
 // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function headerScroll(){
+
     if (window.pageYOffset > sticky) {
         header_nav.classList.add('sticky')
     }
@@ -23,6 +19,7 @@ let btnToTop = document.getElementById('btnToTop');
 
 window.onscroll = function(){
     scrollFunction();
+    headerScroll();
 }
 
 function scrollFunction(){
@@ -39,3 +36,30 @@ function toTopFunction(){
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 }
+
+// click search tour and hotel
+
+let btnSearch = document.querySelector('button.search-btn[search="active"]')
+let btnHotel = document.querySelector('button.search-btn[search=""]')
+let form1 = document.querySelector('form.wp-search-content[form="active"]')
+let form2 = document.querySelector('form.wp-search-content[form=""]')
+
+btnSearch.onclick = function changeBoxSearch(){
+    // form.classList.add("active");
+    btnHotel.classList.remove('btn--active')
+    btnSearch.classList.add('btn--active')
+    form1.classList.add('active','show')
+    form2.classList.remove('active','show')
+}
+
+btnHotel.onclick = function changeBoxSearch(){
+    btnSearch.classList.remove('btn--active')
+    btnHotel.classList.add('btn--active')
+    form2.classList.add('active','show')
+    form1.classList.remove('active','show')
+}
+
+console.log(form1)
+console.log(form2)
+
+
